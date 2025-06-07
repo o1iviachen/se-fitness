f8cfff55c6fe7f4afc03b40b48dc9df9f646e390
o1iviachen <olivia63chen@gmail.com>
2025-06-07 12:01:08 -0400

    Initial Commit

---
+++ se-fitness/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  se-fitness
+//
+//  Created by olivia chen on 2025-06-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ se-fitnessTests/se_fitnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  se_fitnessTests.swift
+//  se-fitnessTests
+//
+//  Created by olivia chen on 2025-06-07.
+//
+
+import Testing
+@testable import se_fitness
+
+struct se_fitnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
